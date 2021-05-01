d207c6001b6e504134faeac75515bfe87c78a3c3
Report-123 <Report-123@mail.ru>
2021-05-01 23:32:49 +0300

    Add ForecastApplication

--- app/src/main/java/com/example/weatherapp/ForecastApplication.kt
+++ app/src/main/java/com/example/weatherapp/ForecastApplication.kt
@@ -1,14 +1,29 @@
 package com.example.weatherapp
 
 import android.app.Application
+import com.example.weatherapp.db.ForecastDatabase
+import com.example.weatherapp.network.*
+import com.example.weatherapp.repository.ForecastRepository
+import com.example.weatherapp.repository.ForecastRepositoryImpl
 import org.kodein.di.Kodein
 import org.kodein.di.KodeinAware
 import org.kodein.di.android.x.androidXModule
+import org.kodein.di.generic.bind
+import org.kodein.di.generic.instance
+import org.kodein.di.generic.singleton
 
 class ForecastApplication : Application(), KodeinAware {
     override val kodein = Kodein.lazy {
         import(androidXModule(this@ForecastApplication))
-    }
+
+        bind() from singleton { ForecastDatabase(instance()) }
+        bind() from singleton { instance<ForecastDatabase>().currentWeatherDao()}
+        bind<ConnectInterceptor>() with singleton { ConnectInterceptorImpl(instance()) }
+        bind() from singleton { APIService(instance()) }
+        bind<WeatherNetDataSource>() with singleton {WeatherNetDataSourceImpl(instance())}
+        bind<ForecastRepository>() with singleton { ForecastRepositoryImpl(instance(),instance()) }
+
+
 
     }
-}
+  }
