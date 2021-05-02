132c8cdc4c32f681a9f6afec4cdfdc460e81ae0a
Report-123 <Report-123@mail.ru>
2021-05-02 16:17:57 +0300

    Add Today_Layout

--- app/src/main/java/com/example/weatherapp/ForecastApplication.kt
+++ app/src/main/java/com/example/weatherapp/ForecastApplication.kt
@@ -5,12 +5,14 @@ import com.example.weatherapp.db.ForecastDatabase
 import com.example.weatherapp.network.*
 import com.example.weatherapp.repository.ForecastRepository
 import com.example.weatherapp.repository.ForecastRepositoryImpl
+import com.example.weatherapp.ui.Today.TodayViewModelFactory
 import com.jakewharton.threetenabp.AndroidThreeTen
 import org.kodein.di.Kodein
 import org.kodein.di.KodeinAware
 import org.kodein.di.android.x.androidXModule
 import org.kodein.di.generic.bind
 import org.kodein.di.generic.instance
+import org.kodein.di.generic.provider
 import org.kodein.di.generic.singleton
 
 class ForecastApplication : Application(), KodeinAware {
@@ -23,6 +25,7 @@ class ForecastApplication : Application(), KodeinAware {
         bind() from singleton { APIService(instance()) }
         bind<WeatherNetDataSource>() with singleton {WeatherNetDataSourceImpl(instance())}
         bind<ForecastRepository>() with singleton { ForecastRepositoryImpl(instance(),instance()) }
+        bind() from provider {TodayViewModelFactory(instance())  }
     }
     override fun onCreate() {
         super.onCreate()
--- app/src/main/java/com/example/weatherapp/ui/Today/TodayFragment.kt
+++ app/src/main/java/com/example/weatherapp/ui/Today/TodayFragment.kt
@@ -6,18 +6,36 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.ViewModelProviders.of
+import androidx.lifecycle.ViewModelStores.of
 import com.example.weatherapp.R
 import com.example.weatherapp.network.APIService
 import com.example.weatherapp.network.ConnectInterceptorImpl
 import com.example.weatherapp.network.WeatherNetDataSourceImpl
+import com.example.weatherapp.ui.base.FragmentScoped
 import kotlinx.android.synthetic.main.today_fragment.*
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.launch
+import org.kodein.di.Kodein
+import org.kodein.di.KodeinAware
+import org.kodein.di.android.closestKodein
+import org.kodein.di.android.x.closestKodein
+import org.kodein.di.generic.instance
+import retrofit2.Invocation.of
+import java.util.EnumSet.of
+import java.util.Optional.of
+
 //const val TAG = "MainActivity"
 
-class TodayFragment : Fragment() {
+class TodayFragment : FragmentScoped(), KodeinAware {
+override val kodein by closestKodein()
+private val viewModelFactory: TodayViewModelFactory by instance()
 
+    companion object {
+        fun newInstace()=TodayFragment
+    }
     private lateinit var viewModel: TodayViewModel
 
     override fun onCreateView(
@@ -26,21 +44,32 @@ class TodayFragment : Fragment() {
     ): View? {
         return inflater.inflate(R.layout.today_fragment, container, false)
     }
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        val apiService = APIService(ConnectInterceptorImpl(this.requireContext()))
-        val weatherNetDataSource = WeatherNetDataSourceImpl(apiService)
-        weatherNetDataSource.downloadedCurrentWeather.observe(viewLifecycleOwner, Observer {
-            tv.text = it.toString()
-//            tv.text = weatherHistory?.current.toString()
-        })
-        GlobalScope.launch (Dispatchers.Main){
-        weatherNetDataSource.fetchCurrentWeather(53.9,27.56,1619481600)
-        }
+        viewModel=ViewModelProvider(this, viewModelFactory).get(TodayViewModel::class.java)
+        bindUI()
+//        val apiService = APIService(ConnectInterceptorImpl(this.requireContext()))
+//        val weatherNetDataSource = WeatherNetDataSourceImpl(apiService)
+//        weatherNetDataSource.downloadedCurrentWeather.observe(viewLifecycleOwner, Observer {
+//            tv.text = it.toString()
+////            tv.text = weatherHistory?.current.toString()
+//        })
+//        GlobalScope.launch (Dispatchers.Main){
+//        weatherNetDataSource.fetchCurrentWeather(53.9,27.56,1619481600)
+//        }
 //        getHistoricalWeather()
-
     }
 
+    private fun bindUI()=launch {
+        val todayWeather=viewModel.weather.await()
+        todayWeather.observe(viewLifecycleOwner, Observer {
+            if (it==null)
+                return@Observer
+            tv.text=it.toString()
+        })
+    }
+//observ Activ
 //    private fun getHistoricalWeather() {
 //        lifecycleScope.launchWhenCreated {
 //            val response = try {
