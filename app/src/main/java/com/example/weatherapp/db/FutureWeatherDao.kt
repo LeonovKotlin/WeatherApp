79d6ada0efc120da3f7c5d383622723883061ea7
Report-123 <Report-123@mail.ru>
2021-05-13 03:50:05 +0300

    add FutureDao

---
+++ app/src/main/java/com/example/weatherapp/db/FutureWeatherDao.kt
@@ -0,0 +1,29 @@
+package com.example.weatherapp.db
+
+import androidx.lifecycle.LiveData
+import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+import com.example.weatherapp.db.entities.future.ForecastWeatherEntry
+import com.example.weatherapp.db.unitlocalized.future.ImperialFutureWeather
+import com.example.weatherapp.db.unitlocalized.future.MetricFutureWeather
+import org.threeten.bp.LocalDate
+
+@Dao
+interface FutureWeatherDao {
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    fun insert(forecastWeatherEntry: List<ForecastWeatherEntry>)
+
+    @Query("select * from future_weather where dtTxt(dtTxt) >= data(:startDate)")
+    fun getFutureWeatherMetric(startDate: LocalDate) : LiveData<List<MetricFutureWeather>>
+
+    @Query("select * from future_weather where dt(dt) >= data(:startDate)")
+    fun getFutureWeatherImperial(startDate: LocalDate) : LiveData<List<ImperialFutureWeather>>
+
+    @Query("select count(id) from future_weather where dt(dt) >= data(:startDate)")
+    fun countFutureWeather(startDate: LocalDate) : Int
+
+    @Query("delete from future_weather where dt(dt) < data(:firstDateToKeep)")
+    fun deleteOldEntries(firstDateToKeep: LocalDate)
+}
