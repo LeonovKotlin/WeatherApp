88ae6085ecf08574acea8129befe2bca1545131f
Report-123 <Report-123@mail.ru>
2021-05-14 03:56:19 +0300

    Future Forecast-future fragment(map to list-convert)

--- app/src/main/java/com/example/weatherapp/ui/FutureForecast/ForecastFragment.kt
+++ app/src/main/java/com/example/weatherapp/ui/FutureForecast/ForecastFragment.kt
@@ -11,6 +11,7 @@ import androidx.lifecycle.ViewModelProvider
 import com.example.weatherapp.R
 import com.example.weatherapp.databinding.ForecastFragmentBinding
 import com.example.weatherapp.databinding.TodayFragmentBinding
+import com.example.weatherapp.db.unitlocalized.future.SpeceficFutureWeather
 import com.example.weatherapp.ui.base.FragmentScoped
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
@@ -50,10 +51,22 @@ class ForecastFragment : FragmentScoped(), KodeinAware {
             binding.loading.visibility = View.GONE
 
             updateDateToFiveDays()
+            initRecyclerView(weatherEntries.toFutureWeatherItems())
         })
     }
     private fun updateLocation(location: String) {
         (activity as? AppCompatActivity)?.supportActionBar?.title = location
     }
+    private fun updateDateToFiveDays() {
+        (activity as? AppCompatActivity)?.supportActionBar?.subtitle = "FiveDays"
+    }
+    private fun List<SpeceficFutureWeather>.toFutureWeatherItems() : List<FutureWeatherItem> {
+        return this.map {
+            FutureWeatherItem(it)
+        }
+    }
+    private fun initRecyclerView(item: List<FutureWeatherItem>) {
 
+
+}
 }
